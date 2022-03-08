b088fbebb015feade0fee9bdc468bde9192dedf5
Mark Shaw <93953616+AboutShaw@users.noreply.github.com>
2022-03-08 10:41:36 +0000

    Clean before starting branches

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,29 @@ export default function Header(setTopic) {
         <header id="mainHeader">
                 <h1>Prime News</h1>
                 <h2>The news you didn't want!</h2>
-            <nav id="navBar">
-                <Link to="/" >Home</Link>
-                <Link to="/coding" onClick={() => {setTopic("coding")}}>Coding</Link>
-                <Link to="/football" onClick={() => {setTopic("football")}}>Football</Link>
-                <Link to="/cooking" onClick={() => {setTopic("cooking")}}>Cooking</Link>
-            </nav>
-
+                <div id="navSort">
+                    <nav id="navBar">
+                        <Link to="/" >Home</Link>
+                        <Link to="/coding" onClick={() => {setTopic("coding")}}>Coding</Link>
+                        <Link to="/football" onClick={() => {setTopic("football")}}>Football</Link>
+                        <Link to="/cooking" onClick={() => {setTopic("cooking")}}>Cooking</Link>
+                    </nav>
+                    <div id="sorterContainer">
+                        <p>Title:</p>
+                        <Link to="" >↟</Link>
+                        <Link id="aRight" to="" >↡</Link>
+                        <p>Author:</p>
+                        <Link to="" >↟</Link>
+                        <Link id="aRight" to="" >↡</Link>
+                        <p>Votes:</p>
+                        <Link to="" >↟</Link>
+                        <Link id="aRight" to="" >↡</Link>
+                        <p>Comments:</p>
+                        <Link to="" >↟</Link>
+                        <Link id="aRight" to="" >↡</Link>
+                    </div>
+                </div>
+            
         </header>
     )
 }
