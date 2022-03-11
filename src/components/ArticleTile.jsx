44f00b6a2bbcdada26d02be6e22801a62f02c0dd
Mark Shaw <93953616+AboutShaw@users.noreply.github.com>
2022-03-11 15:38:00 +0000

    clean

--- src/components/ArticleTile.jsx
+++ src/components/ArticleTile.jsx
@@ -1,20 +1,27 @@
 import moment from "moment";
 import { Link } from "react-router-dom";
-export default function ArticleCard({ article_id, title, topic, author, body, created_at, votes, comment_count }) {
-    
-const topicFormat = topic.toUpperCase()
-const date = moment(created_at).format("MMM Do YY")
+export default function ArticleCard({
+  article_id,
+  title,
+  topic,
+  author,
+  created_at,
+  votes,
+  comment_count,
+}) {
+  const topicFormat = topic.toUpperCase();
+  const date = moment(created_at).format("MMM Do YY");
 
-    return (
-      <article id={article_id}>
-        <Link to={`/articles/${article_id}`}>{title}</Link>
-        <div className="infoHolder">
-          <p>Topic: {topicFormat}</p>
-          <p>Author: {author}</p>
-          <p>Submitted: {date}</p>
-          <p>Votes: {votes}</p>
-          <p>Comments: {comment_count}</p>
-        </div>
-      </article>
-    );
-  }
+  return (
+    <article id={article_id}>
+      <Link to={`/articles/${article_id}`}>{title}</Link>
+      <div className="infoHolder">
+        <p>Topic: {topicFormat}</p>
+        <p>Author: {author}</p>
+        <p>Submitted: {date}</p>
+        <p>Votes: {votes}</p>
+        <p>Comments: {comment_count}</p>
+      </div>
+    </article>
+  );
+}
