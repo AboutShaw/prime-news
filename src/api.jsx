dacac38f050788a79c6b081a6885c0c29c39edec
Mark Shaw <93953616+AboutShaw@users.noreply.github.com>
2022-03-11 11:35:47 +0000

    Able to post commetns on article 1-10

--- src/api.jsx
+++ src/api.jsx
@@ -1,38 +1,41 @@
 import axios from "axios";
 
 const newsApi = axios.create({
-    baseURL: 'https://mshaw-nc-news.herokuapp.com/api'
-  });
+  baseURL: "https://mshaw-nc-news.herokuapp.com/api",
+});
 
-const getArticles = (topic, sort_by, order_by) => { 
-    console.log(topic, sort_by, order_by, "api")
-    return newsApi.get(`/articles`, {params: {topic, sort_by, order_by}})
-        .then((response) => {
-            return response.data
-        })
+const getArticles = (topic, sort_by, order_by) => {
+  console.log(topic, sort_by, order_by, "api");
+  return newsApi
+    .get(`/articles`, { params: { topic, sort_by, order_by } })
+    .then((response) => {
+      return response.data;
+    });
 };
 
 const getArticle = (article_id) => {
-    return newsApi.get(`/articles/${article_id}`)
-    .then((response) => {
-        return response.data.article
-    })
-}
+  return newsApi.get(`/articles/${article_id}`).then((response) => {
+    return response.data.article;
+  });
+};
 
 const getTopics = () => {
-    return newsApi.get(`/topics`)
-    .then((response) => {
-        return response.data
-    })
-}
+  return newsApi.get(`/topics`).then((response) => {
+    return response.data;
+  });
+};
 
 const getComments = (article_id) => {
-    console.log(article_id, "getCom ln30")
-    return newsApi.get(`/articles/${article_id}/comments`)
-    .then((response) => {
-        return response.data.comments
-    })
-}
+  return newsApi.get(`/articles/${article_id}/comments`).then((response) => {
+    return response.data.comments;
+  });
+};
 
-export { getArticles, getTopics, getArticle, getComments};
+const postComment = (article_id, body) => {
+  return newsApi.post(`/articles/${article_id}/comments`, {
+    username: "jessjelly",
+    body: body,
+  });
+};
 
+export { getArticles, getTopics, getArticle, getComments, postComment };
